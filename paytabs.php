f9bd2903810cda23481efa2a58c4ef5d948ed54a
Mohamed-Hamed999 <148044093+Mohamed-Hamed999@users.noreply.github.com>
2024-09-24 13:53:50 +0300

    add kuwait branch (#13)
    * add Kuwait endpoint

--- paytabs.php
+++ paytabs.php
@@ -2,7 +2,7 @@
 
 /**
  * Name:    PayTabs payment gateway
- * Version: 3.3.1
+ * Version: 3.4.0
  */
 
 if (!defined("WHMCS")) {
@@ -10,7 +10,7 @@ if (!defined("WHMCS")) {
 }
 
 
-define('PAYTABS_PAYPAGE_VERSION', '3.3.1');
+define('PAYTABS_PAYPAGE_VERSION', '3.4.0');
 require_once 'paytabs_files/paytabs_core.php';
 require_once 'paytabs_files/paytabs_functions.php';
 
